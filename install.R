f9611891b26061c367de31c17091a193b27a4461
oxbrookes04 <oxbrookes04@gmail.com>
2025-11-19 14:50:24 +0800

    Update install.R

--- install.R
+++ install.R
@@ -1,2 +1,8 @@
 install.packages("BiocManager")
-BiocManager::install("Biostrings")
+BiocManager::install(version = "3.18")
+BiocManager::install(c(
+  "memes",
+  "BSgenome.Hsapiens.UCSC.hg19",
+  "GenomicRanges",
+  "Biostrings"
+))

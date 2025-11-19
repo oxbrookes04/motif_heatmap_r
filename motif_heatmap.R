761d85ef9838d6a5b9f4631d7fda00d16a31967a
oxbrookes04 <oxbrookes04@gmail.com>
2025-11-19 15:15:40 +0800

    Update motif_heatmap.R

--- motif_heatmap.R
+++ motif_heatmap.R
@@ -1,22 +1,8 @@
-library(memes)
-library(BSgenome.Hsapiens.UCSC.hg19)
-library(GenomicRanges)
-library(Biostrings)
+# Load ggplot2
+library(ggplot2)
 
-# Load BED-like file
-bed <- read.delim("Motif_windows_30bp.tsv", header = TRUE)
-
-# Convert to GRanges
-gr <- GRanges(seqnames = bed$Chr,
-              ranges = IRanges(start = bed$Start, end = bed$End),
-              strand = bed$motif_strand,
-              gene = bed$gene)
-
-# Load genome
-genome <- BSgenome.Hsapiens.UCSC.hg19
-
-# Extract sequences
-seqs <- get_sequence(gr, genome)
-
-# Plot motif-centered heatmap
-plot_sequence_heatmap(seqs)
+# Create a simple plot
+df <- data.frame(x = 1:10, y = (1:10)^2)
+ggplot(df, aes(x = x, y = y)) +
+  geom_line(color = "blue") +
+  ggtitle("Test Plot: y = x^2")
